afad1660b8c7d84ba2940d277c5e51906ed59fc7
akasifislam <aseasifislam@gmail.com>
2021-10-30 23:25:02 +0600

    message chat

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -20,17 +20,24 @@ class MessageController extends Controller
 
     public function user_list()
     {
-        // if (\Request::ajax()) {
-        $userlist = User::latest()->paginate(9);
-        return response()->json($userlist, 200);
-        // }
-        // return abort(404);
+        if (\Request::ajax()) {
+            $userlist = User::latest()->paginate(9);
+            return response()->json($userlist, 200);
+        }
+        return abort(404);
     }
 
 
     public function user_message($id = null)
     {
-        $message = Message::where('form', $id)->get();
-        return response()->json($message);
+
+        // if (\Request::ajax()) {
+        $message = Message::where(function ($q) use ($id) {
+            // $q->where('form', $id);
+            $q->where('to', $id);
+        })->get();
+        return response()->json($message, 200);
+        // }
+        // return abort(404);
     }
 }
