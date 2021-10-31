502930714e0e1120bee251234c56edb7b4b7568c
akasifislam <aseasifislam@gmail.com>
2021-10-31 22:25:57 +0600

    tets

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Message;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
 <main-app></main-app>
+
 @endsection
