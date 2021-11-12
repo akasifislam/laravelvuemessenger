9cbb50a25e8544d28d540daaa844bc9de7152eda
akasifislam <aseasifislam@gmail.com>
2021-11-12 22:21:00 +0600

    pagination remove

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -26,7 +26,7 @@ class MessageController extends Controller
         // $notuser = auth()->user()->id;
         // return $user = User::where('id', !auth()->user()->id)->get();
         if (\Request::ajax()) {
-            $userlist = User::latest()->where('id', '!=', auth()->user()->id)->paginate(10);
+            $userlist = User::latest()->where('id', '!=', auth()->user()->id)->get();
             return response()->json($userlist, 200);
         }
         return abort(404);
